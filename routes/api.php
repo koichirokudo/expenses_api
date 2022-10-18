e66b067dda0ef52595877bf872986b4f62164046
koichirokudo <koichirokudo746@gmail.com>
2022-10-18 23:12:50 +0900

    Add route
    * routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExpensesController;
@@ -19,4 +20,8 @@ Route::post('/user/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 // sanctum 認証ガードを使用することにより、認証済みのユーザーのみがアクセス可能
 Route::get('/user/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+Route::get('/user/index', [UserController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/expenses/index', [ExpensesController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/expenses/spend/month', [ExpensesController::class, 'spend'])->middleware('auth:sanctum');
+Route::get('/expenses/income/month', [ExpensesController::class, 'income'])->middleware('auth:sanctum');
 Route::post('/expenses/register', [ExpensesController::class, 'register'])->middleware('auth:sanctum');
